94e8a138c76a409a525ffa2e0223642f45aa1c59
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-08-20 16:57:01 -0500

    almost done but can't add more than one song

--- javascripts/add.js
+++ javascripts/add.js
@@ -9,19 +9,42 @@ var addView = document.getElementById("add-view");
 addLink.addEventListener("click", function() {
   homeView.classList.add("hidden");
   listView.classList.add("hidden");
-
+  
   addView.classList.add("visible");
   addView.classList.remove("hidden");
-
+  
+  var songNameInput = document.getElementById("songName");
+  var artistInput = document.getElementById("artistName");
+  var albumInput = document.getElementById("albumName");
+  var inputButton = document.getElementById("add-button");
+      function pushToArray() {
+        console.log("hi");
+        songs2.push({name: songNameInput.value, artist: artistInput.value, album: albumInput.value, genre: "Whatever"}); // Array now contains 1 item
+      }
+  console.log(songs2);
+  inputButton.addEventListener("click", function(){
+    console.log("omggg");
+    pushToArray();
+    displaySongs();
+  });
 });
 
+console.log("hi there");
+
+
+  // inputButton.addEventListener("click", function() {
+  //   console.log("ughhhh");
+  // })
+  // console.log(inputButton);
+function displaySongs() {
 for (let j = 0; j < songs2.length; j++) {
     
-    songs2.push({name: "Whatever", artist: "Whoever", album: "Whatever", genre: "Whatever"}); // Array now contains 1 item
-    contentArea2.innerHTML += `<div class="Song1 specific-song">
-                              <h2>${songs2[j].name}</h2>
-                              <h4 class="song1list artist-name">${songs2[j].artist}</h4>
-                              <h4 class="song1list album-name">${songs2[j].album}</h4>
-                              <h4 class="song1list genre-type">${songs2[j].genre}</h4>
-                              </div>`;
-    }
+  contentArea2.innerHTML += `<div class="Song1 specific-song">
+                            <h2>${songs2[j].name}</h2>
+                            <h4 class="song1list artist-name">${songs2[j].artist}</h4>
+                            <h4 class="song1list album-name">${songs2[j].album}</h4>
+                            <h4 class="song1list genre-type">${songs2[j].genre}</h4>
+                            </div>`;
+                            console.log(songs2[j]);
+}
+}
--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,39 +1,5 @@
 console.log("hi");
 
-let songs = [];
-
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-// 
-songs.unshift("Karma Police - by RadioHead on the album OK Computer");
-
-// console.log(songs);
-
-songs.push("Tiny Dancer - by Elton John on the album Madman Across the Water");
-
-// console.log(songs);
-
-// songs.splice(0, 2, "*");
-// let replaceCharacters = "";
-for (let i = 0; i < songs.length; i++) {
-	// console.log(songs[i]);
-	// replaceCharacters = songs[i].indexOf(">") - 1;
-	// console.log(replaceCharacters);
-	songs[i] = songs[i].replace('*', '');
-		// console.log(songs[i]);
-	songs[i] = songs[i].replace('!', '');
-	songs[i] = songs[i].replace('(', '');
-	songs[i] = songs[i].replace('@', '');
-	songs[i] = songs[i].replace('>', '-');
-	// console.log(songs[i]);
-	// contentArea.innerHTML += `<div class="specific-song">
-	// 										<h4>${songs[i]}</h4>
-	// 								</div>`;
-}
 
 let songs2 = [
 { 
@@ -66,6 +32,8 @@ genre: "Alternative"
 let contentArea = document.getElementById("song-container");
 let contentArea2 = document.getElementById("song-container2");
 
+
+
 for (i = 0; i < songs2.length; i++) {
 	console.log(songs2[i]);
 	contentArea.innerHTML += `<div class="Song1 specific-song">
@@ -75,28 +43,5 @@ for (i = 0; i < songs2.length; i++) {
 								<h4 class="song1list genre-type">${songs2[i].genre}</h4>
 								</div>`;
 
-	contentArea2.innerHTML += `<div class="Song1 specific-song">
-								<h2>${songs2[i].name}</h2>
-								<h4 class="song1list artist-name">${songs2[i].artist}</h4>
-								<h4 class="song1list album-name">${songs2[i].album}</h4>
-								<h4 class="song1list genre-type">${songs2[i].genre}</h4>
-								</div>`;
+	
 }
-// console.log(songs);
-
-// songs.replace(/r/g, '');
-
-// console.log(songs);
-
-// function mySongs(){
-	// let index = songs.indexOf("Legs");
-	// console.log(index);
-	// let length = Sonnet.length;
-	// console.log(length);
-	// let characterReplace = songs.replace("Legs", "Movie");
-	// document.getElementById("sonnet").innerHTML = winSwitch;
-	// let largeSwitch = winSwitch.replace(/ the /gi, " a large ");
-	// document.getElementById("sonnet").innerHTML = largeSwitch;
-// }
-
-// mySongs(songs)
