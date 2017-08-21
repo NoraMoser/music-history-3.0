3466828a8ed907f9da9351530ebc47bac6f78a22
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-08-21 09:09:16 -0500

    still just the first

--- javascripts/add.js
+++ javascripts/add.js
@@ -13,18 +13,11 @@ addLink.addEventListener("click", function() {
   addView.classList.add("visible");
   addView.classList.remove("hidden");
   
-  var songNameInput = document.getElementById("songName");
-  var artistInput = document.getElementById("artistName");
-  var albumInput = document.getElementById("albumName");
   var inputButton = document.getElementById("add-button");
-      function pushToArray() {
-        console.log("hi");
-        songs2.unshift({name: songNameInput.value, artist: artistInput.value, album: albumInput.value, genre: "Whatever"}); // Array now contains 1 item
-      }
+
   console.log(songs2);
   inputButton.addEventListener("click", function(){
     console.log("omggg");
-    pushToArray();
     displaySongs();
   });
 });
@@ -32,13 +25,15 @@ addLink.addEventListener("click", function() {
 console.log("hi there");
 
 
-  // inputButton.addEventListener("click", function() {
-  //   console.log("ughhhh");
-  // })
-  // console.log(inputButton);
 function displaySongs() {
-for (let j = 0; j < songs2.length; j++) {
-    
+  var songNameInput = document.getElementById("songName");
+  var artistInput = document.getElementById("artistName");
+  var albumInput = document.getElementById("albumName");
+
+  songs2.unshift({name: songNameInput.value, artist: artistInput.value, album: albumInput.value, genre: "Whatever"}); // Array now contains 1 item
+  for (let j = 0; j < songs2.length; j++) {
+  
+  console.log(songs2[j]);
   contentArea2.innerHTML += `<div class="Song1 specific-song">
                             <h2>${songs2[j].name}</h2>
                             <h4 class="song1list artist-name">${songs2[j].artist}</h4>
